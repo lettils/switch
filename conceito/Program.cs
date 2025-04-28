cfc8dc06ebc681a6e95a48559ce62ef48830dcfc
Leticia Lima <leticiaalima77@gmail.com>
2025-04-28 10:30:06 -0300

    escola

---
+++ conceito/Program.cs
@@ -0,0 +1,35 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+
+
+        Console.WriteLine("Digite um Letra de A ate E ");
+       char  informacao = Convert.ToChar(Console.ReadLine());
+
+        switch (informacao)
+        {
+            case 'A':
+                Console.WriteLine("Excelente");
+                break;
+            case 'B':
+                Console.WriteLine("Bom");
+                break;
+            case 'C':
+                Console.WriteLine("Regular");
+                break;
+            case 'D':
+                Console.WriteLine("Ruim");
+                break;
+            case 'E':
+                Console.WriteLine("pessimo");
+                break;
+           
+
+        }
+
+
+
+    }
+}
